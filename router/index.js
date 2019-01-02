2b183fb401207c5f939c3a3d98c31d6357032b4d
ikegam1 <41247620+ikegam1@users.noreply.github.com>
2019-01-02 23:44:26 +0900

    Update index.js

--- router/index.js
+++ router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import VideoStream from '@/components/VideoStream'
 
 Vue.use(Router)
 
@@ -8,8 +8,9 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'VideoStream',
+      component: VideoStream
     }
   ]
 })
+
